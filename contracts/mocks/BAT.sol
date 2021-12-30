4f162d54749f7d0f62b5eea5dbb1c6b9b2fd52e8
julien <julien.thomas84@gmail.com>
2021-12-30 18:42:25 -0500

    feat(mocks): add 2 other ERC20 mocks: BAT & WETH

--- contracts/mocks/BAT.sol
+++ contracts/mocks/BAT.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DAI is ERC20 {
-    constructor() ERC20("DAI Stablecoin", "DAI") {}
+contract BAT is ERC20 {
+    constructor() ERC20("BAT", "BAT") {}
 
     function faucet(address to, uint amount) external {
         _mint(to, amount);
